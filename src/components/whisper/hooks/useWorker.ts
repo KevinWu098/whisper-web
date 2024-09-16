5584f24a56668fcce3c84146f0606c02100334fe
Kevin Wu <kevinwu098@gmail.com>
2024-09-16 02:17:33 -0400

    feat: initial commit

---
+++ src/components/whisper/hooks/useWorker.ts
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export interface MessageEventHandler {
+  (event: MessageEvent): void;
+}
+
+export function useWorker(messageEventHandler: MessageEventHandler): Worker {
+  // Create new worker once and never again
+  const [worker] = useState(() => createWorker(messageEventHandler));
+  return worker;
+}
+
+function createWorker(messageEventHandler: MessageEventHandler): Worker {
+  const worker = new Worker(
+    // whisper-worker.js should be at the root of `src`
+    new URL("../../../../whisper-worker.js", import.meta.url),
+    {
+      type: "module",
+    },
+  );
+  // Listen for messages from the Web Worker
+  worker.addEventListener("message", messageEventHandler);
+  return worker;
+}
